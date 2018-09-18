9b40a54661de13492cec54e2d9bba50ba57e3192
Hinatsu Kusano <hinatsukusano@gmail.com>
2018-09-19 00:52:54 +0900

    Modified some parts

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,36 +1,34 @@
 import React from 'react';
 
 const Task = (props) => (
-  <div>
-    <div className="task">
+  <div className="task">
+    <div>
       <div>
-        <div>
-          <input 
-            className="task__Check"
-            type="checkbox" 
-          />
-          <div className="task__text">{props.taskText}</div>
-        </div>
-        
+        <input 
+          className="task__Check"
+          type="checkbox" 
+        />
+        <div className="task__text">{props.taskText}</div>
       </div>
-      <div>
-        <button
-          className="button button--link"
-          onClick={(e) => {
-            props.handleEditTask(props.taskText);
-          }}
-        >
-          Edit
-        </button>
-        <button 
-          className="button button--link button--edit"
-          onClick={(e) => {
-            props.handleDeleteTask(props.taskText);
-          }}
-        >
-          Remove
+      
+    </div>
+    <div>
+      <button
+        className="button button--link"
+        onClick={(e) => {
+          props.handleEditTask(props.taskText);
+        }}
+      >
+        Edit
       </button>
-      </div>
+      <button 
+        className="button button--link button--edit"
+        onClick={(e) => {
+          props.handleDeleteTask(props.taskText);
+        }}
+      >
+        Remove
+      </button>
     </div>
   </div>
 ); 
